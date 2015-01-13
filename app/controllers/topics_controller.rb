ac18ae5ff4d1ea6992e398ee2eaf0f07414b2f5a
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2015-01-13 22:17:19 +0300

    topic#create

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,4 +2,14 @@ class TopicsController < ApplicationController
   def index
     @topics = Topic.all
   end
+
+  def create
+    Topic.create(topic_params)
+    redirect_to root_path
+  end
+
+  private
+  def topic_params
+    params.require(:topic).permit(:title)
+  end
 end
--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -7,4 +7,12 @@ class TopicsControllerTest < ActionController::TestCase
     assert_not_nil assigns :topics
     assert_select 'h1', 'All voting topics'
   end
+
+  test "create topic" do
+    post :create, { topic: { title: "blah" } }
+    assert_redirected_to root_path
+    @topic = Topic.find_by title: "blah"
+    assert_not_nil @topic
+    assert_equal @topic.title, "blah"
+  end
 end
